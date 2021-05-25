79026122880ef2d2daa07394d15c21c711e7f78c
Dileep <65488990+varadidileep@users.noreply.github.com>
2021-05-25 09:46:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React , {Component} from 'react'
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './QuoteMachine/QuoteMachine';
+ 
+
+function App() {
+  return (
+    <div className="App">
+       <Calculator />
+    </div>
+  );
+}
+
+export default App;
