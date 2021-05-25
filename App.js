f97b971715a566881d0d582676384b474021bf5b
Dileep <65488990+varadidileep@users.noreply.github.com>
2021-05-25 09:49:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React , {Component} from 'react'
+import logo from './logo.svg';
+import './App.css';
+import QuoteMachine from './QuoteMachine/QuoteMachine';
+ 
+
+function App() {
+  return (
+    <div className="App">
+       <QuoteMachine />
+    </div>
+  );
+}
+
+export default App;
